9009b22bc633c33ba516e65e03f8a8a0eecd441c
TirzzNesia <106883004+TirzzNesia@users.noreply.github.com>
2025-05-15 00:19:42 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  ownerName: 'HEXAVIS', // Ganti dengan nama owner lu
+  botToken: '7461266218:AAFidrk8qN08BDcH68KOj8yT2wddFwZCOYI', // Ganti dengan token bot Telegram lu
+  owner: '7776909228', //OWNER user id
+};
+
+module.exports = settings;
